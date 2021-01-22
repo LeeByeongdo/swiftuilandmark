d1a5c63de12e57b56b24313ff5c9fc857d10f1a0
byeongdolee <bdbdbdlee@gmail.com>
2021-01-22 16:28:39 +0900

    Initial Commit

---
+++ LandMarks/LandMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarksApp.swift
+//  LandMarks
+//
+//  Created by ByeongdoLee on 2021/01/22.
+//
+
+import SwiftUI
+
+@main
+struct LandMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
